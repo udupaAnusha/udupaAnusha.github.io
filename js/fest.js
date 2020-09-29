a6209cb414180b9e8bafeda70bb748d5f1c7054e
HP <anusha.udupa@gmail.com>
2020-09-29 21:47:43 +0530

    mobel: initial build

---
+++ js/fest.js
@@ -0,0 +1,11 @@
+/** First Slider */
+$('.slider-two')
+.not(".slick-intialized")
+.slick({
+    prevArrow:".site-slider-two .prev",
+    nextArrow:".site-slider-two .next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplaySpeed:3000
+
+});
---
+++ js/main.js
@@ -0,0 +1,13 @@
+/** First Slider */
+$('.slider-one')
+.not(".slick-intialized")
+.slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    dots:true,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+
+});
+
+
